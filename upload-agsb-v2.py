234f533d10093f33d57a86d41cd71b544b8274a2
gongyijun0416 <18071025606@qq.com>
2025-06-06 14:04:35 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
+USER_NAME = "yijun0416"         # 用户名
+UUID = "3afad02e-e873-4843-8da4-ad84c6780f1c"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
 DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+CF_TOKEN = "yJhIjoiMTE3NjBhN2QyNjFhZWFiZWI1MzFlODk3ODcxNTUyYTIiLCJ0IjoiMzE2NzQ3MzUtNzUyOC00NjYwLThkOTctZWQ5ZjA0MzdhZjNhIiwicyI6IlpUWTRaVGd3WlRNdFpXTmxZeTAwTkRNMExXSTFOamN0Wm1FNFpEVTNNV00xTWpRMCJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
